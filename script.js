52f0fc5617fee8c38bdbcf7bb9c4ce22fc728d34
Daevah Taylaran <daevtayl@gmail.com>
2022-05-18 14:23:15 -0700

    added preview and code

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let color2 = document.querySelector(".color2");
 
 let body = document.getElementById("gradient");
 
-console.log(color1);
-console.log(color2.value);
+let degreeInput = document.getElementById("preview-angle");
+let degreeDisplay = document.getElementById("degree-preview");
 
 function changeBackground() {
-  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  body.style.background = `linear-gradient(180deg, ${color1.value}, ${color2.value})`;
   console.log(color2.value);
 }
 
@@ -17,17 +17,33 @@ color1.addEventListener("input", changeBackground);
 color2.addEventListener("input", changeBackground);
 
 
-// bottom right gradient direction sample
+// degree slider
+
+degreeInput.addEventListener("input", previewDegree);
+degreeInput.addEventListener("input", brGradientBg);
+
+function previewDegree() {
+  console.log(degreeInput.value);
+  degreeDisplay.innerText = degreeInput.value + '°';
+}
+
+function grabHex(span, element) {
+  span.innerText = `${element.value}`;
+}
+
+// gradient direction sample
 
 let brGradient = document.getElementById("br-gradient");
 let brColor1 = document.querySelector(".br1-color");
 let brColor2 = document.querySelector(".br2-color");
+let firstHex = document.getElementById("1-hex");
+let secHex = document.getElementById("2-hex");
 
 function brGradientBg() {
-  brGradient.style.background = `linear-gradient(to bottom right, ${brColor1.value}, ${brColor2.value})`;
+  brGradient.style.background = `linear-gradient(${degreeInput.value}deg, ${brColor1.value}, ${brColor2.value})`;
+  grabHex(firstHex, brColor1);
+  grabHex(secHex, brColor2);
 }
 
 brColor1.addEventListener("input", brGradientBg);
 brColor2.addEventListener("input", brGradientBg);
-
-
